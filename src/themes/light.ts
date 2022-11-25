e6f6ab51965a80c9368212c8a7854e6c141578b6
Clover <cloveryou02@163.com>
2022-11-25 11:56:46 +0800

    添加高亮主题

---
+++ src/themes/light.ts
@@ -0,0 +1,17 @@
+/**
+ * <p>
+ * 默认主题
+ * </p>
+ *
+ * @version: v1.0
+ * @author: Clover
+ * @create: 2022-11-25 11:37
+ */
+import { theme } from 'antd'
+import { AliasToken } from 'antd/es/theme'
+
+const { defaultSeed } = theme
+
+export const lightTheme: Partial<AliasToken> = {
+  colorPrimary: '#00a846'
+}
