5578458ad1448508bc02ce6e34c51ff72f593864
Clover <cloveryou02@163.com>
2022-11-25 11:56:59 +0800

    添加登录页面

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,59 @@
+/**
+ * <p>
+ * 登录页
+ * </p>
+ *
+ * @version: v1.0
+ * @author: Clover
+ * @create: 2022-11-25 11:42
+ */
+import { Button, Checkbox, Form, Input } from 'antd';
+
+export const LoginPage: React.FC = () => {
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return <>
+    <Form
+      name="basic"
+      labelCol={{ span: 9 }}
+      wrapperCol={{ span: 7 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="用户名"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="密码"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 0, span: 0 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 0, span: 0 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  </>
+}
