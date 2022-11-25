918d8a562ac410cc17e95af5cd79710542bba9d8
Clover <cloveryou02@163.com>
2022-11-25 11:57:28 +0800

    配置antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { ConfigProvider } from "antd";
+
+import * as theme from '@/themes/light'
+
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import { Button, Checkbox, Form, Input } from 'antd';
 import "./App.css";
+import { LoginPage } from "./pages/login";
 
 const App: React.FC = () => {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,83 +17,28 @@ const App: React.FC = () => {
     setGreetMsg(await invoke("greet", { name }));
   }
 
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
+  return <>
+    <React.StrictMode>
+      <ConfigProvider theme={{ token: theme.lightTheme }}>
+        <div className="container">
+          <h1>Welcome to Tauri!</h1>
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
-
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-{/* 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+          <div className="row">
+            <a href="https://vitejs.dev" target="_blank">
+              <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+            </a>
+            <a href="https://tauri.app" target="_blank">
+              <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+            </a>
+            <a href="https://reactjs.org" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
         </div>
-      </div>
-      <p>{greetMsg}</p> */}
-
-      <Form
-      name="basic"
-      labelCol={{ span: 9 }}
-      wrapperCol={{ span: 7 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="用户名"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="密码"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 0, span: 0 }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 0, span: 0 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-    </div>
-  );
+        <LoginPage/>
+      </ConfigProvider>
+    </React.StrictMode>
+  </>
 }
 
 export default App;
