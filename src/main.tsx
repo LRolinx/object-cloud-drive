392186a97c4edfccb4d7604cc48ae8428524d999
Clover <cloveryou02@163.com>
2022-11-25 13:34:14 +0800

    修改名称

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {StyleProvider} from "@ant-design/cssinjs"
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./style.css";
+import "./style.less";
 
 export default ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
