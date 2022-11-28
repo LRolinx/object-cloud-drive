f0e3adaa49539cdbb27bff7bb48507ae87f4619a
Clover <cloveryou02@163.com>
2022-11-28 23:56:26 +0800

    feat: 添加显示当前窗口方法

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,6 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -27,18 +25,17 @@ fn hide_win(window: tauri::Window) {
     window.hide().unwrap();
 }
 
+/**
+ * 显示隐藏窗口
+ */
+#[tauri::command]
+fn show_win(window: tauri::Window) {
+    window.show().unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let login_win = app.get_window("login").unwrap();
-            // 等待500毫秒显示登录页面，防止出现白屏
-            tauri::async_runtime::spawn(async move {
-                std::thread::sleep(std::time::Duration::from_millis(500));
-                login_win.show().unwrap();
-            });
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![greet, close_win, hide_win])
+        .invoke_handler(tauri::generate_handler![greet, close_win, hide_win, show_win])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
